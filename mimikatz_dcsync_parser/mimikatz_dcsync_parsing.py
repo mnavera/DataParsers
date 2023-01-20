b0e99fe1b0568c1e9bf580c9581dc89e47f14690
mnavera <mark.navera00@gmail.com>
2023-01-20 14:29:36 +0800

    finished grouping function

--- mimikatz_dcsync_parser/mimikatz_dcsync_parsing.py
+++ mimikatz_dcsync_parser/mimikatz_dcsync_parsing.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import csv
 import os
-import pandas
+import pandas,numpy
 import argparse
 
+
+pandas.set_option('mode.chained_assignment', None) 
 
 WORKING_FILE='temp.csv'
 EMPTY_LM_HASH='aad3b435b51404eeaad3b435b51404ee'
@@ -11,12 +13,16 @@ EMPTY_LM_HASH='aad3b435b51404eeaad3b435b51404ee'
 parser=argparse.ArgumentParser(description="Transforms csv output from mimikatz lsadump::dcsync /csv into workable formats")
 parser.add_argument('filename')
 parser.add_argument('-c','--crackable',action='store_true',default=False, help='output to john/hashcat crackable format')
-parser.add_argument('-s','--password-stats',action='store_true',default=False,help='parse password statistics')
+parser.add_argument('-s','--password_stats',action='store_true',default=False,help='parse password statistics')
+parser.add_argument('-p','--reused_pws',action='store_true',default=False,help='get reused passwords and output to file')
 parser.add_argument('-b','--hashedby',default='john',help='used with -i, sets format for solved hash file(either john or hashcat, default john)',)
-parser.add_argument('-i','--input_hashes',help='file with solved hashes',)
-parser.add_argument('-r','--regular',action='store_true',default=False,help='perform --crackable and --password-stats')
+parser.add_argument('-i','--solved_hashes',help='file with solved hashes',)
+parser.add_argument('-k','--kerberoastable_users',help='file with kerberoastable users')
+parser.add_argument('-r','--regular',action='store_true',default=False,help='perform --crackable --reused_pws and --password-stats')
 
 args = parser.parse_args()
+
+
 
 def create_working_file(fname):
 	count=0;
@@ -41,6 +47,8 @@ def create_working_file(fname):
 	read_file_handle.close();
 	write_file_handle.close();
 
+
+
 def convert_to_crackable():#convert the working file into a format that can be cracked by john/hashcat
 	with open(WORKING_FILE,'r') as file:
 		csv_reader=csv.DictReader(file,delimiter="\t")
@@ -52,15 +60,60 @@ def convert_to_crackable():#convert the working file into a format that can be c
 
 		output_file.close()
 
-def parse_user_hash_statistics():
-	df=pandas.read_csv(WORKING_FILE,delimiter='\t')
-	unique_hashes=df['nthash'].value_counts()#make Series of all unique hashes and their number of occurences
+
+
+def parse_user_hash_data():
+	df=pandas.read_csv(WORKING_FILE,delimiter='\t').sort_values('nthash')
+	#print(df)
+	reused_pws=df[df['nthash'].map(df['nthash'].value_counts()) > 1]#get only the passwords that are used by more than one user
 	
-#sample hashes
-#440431	liushunlin	1476373a33475c5cd08ad94a6c37d610	514	691
-#976678	yolandalin	32ed87bdb5fdc5e9cba88547376818d4	514	238
+
+	return reused_pws,df
+
+def print_statistics(df):
+	print("\n\n------Statistics-------")
+	print("Total number of users reusing passwords: ",len(df))
+	print("Number of unique passwords in reuse: ",len(df['nthash'].unique()))
+	
+
+def output_to_reportable_format(df,reused):
+	df_grouped=reused.groupby('nthash')['ID'].nunique().sort_values(ascending=False).to_frame().reset_index()#group by unique hash, sort, then make dataframe
+	df_grouped.columns.values[1]="Occurences"
+	df_grouped['Members']='' # add a column to put in list of users who share specific password
+	for index,row in df_grouped.iterrows():
+		member_users=df[df.nthash.isin([row.nthash])]['samaccountname']
+		member_users=''.join(x+'\015' for x in member_users)
+		df_grouped['Members'][index]=member_users
+		
+
+	return df_grouped
+
+	
+
+def main():
+	create_working_file(args.filename)#file that other funcions will refer to
+	reused,df=parse_user_hash_data() #get a separate variable just for the reused pws
+
+	if(args.regular):
+		args.reused_pws,args.crackable,args.password_stats = True,True,True
 
 
-create_working_file(args.filename)
-df=parse_user_hash_statistics()
-os.remove(WORKING_FILE)
+	if(args.reused_pws):
+		print("[+]----Gathering reused password data...")
+		grouped_hashes=output_to_reportable_format(df,reused) #mangle data to the form we want for exporting
+		grouped_hashes.index=numpy.arange(1,len(grouped_hashes)+1)
+		grouped_hashes.to_excel(args.filename.split(".")[0]+'_grouped_byhash.xlsx')
+
+
+	if(args.crackable):
+		print("[+]----Converting to crackable format...")
+		convert_to_crackable()
+
+	if(args.password_stats):
+		print("[+]----Printing statistics...")
+		print_statistics(reused)
+
+
+	os.remove(WORKING_FILE)
+
+main()
